b97f5b15482712e462519c3829ecf2644fc87978
Olesj Bilous <olesj.bilous@vdabcampus.be>
2022-01-05 17:47:24 +0100

    deserialize gebruiker werpt exception: persoon is abstract

---
+++ ForumClient/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using ForumData.Entities;
+using ForumData.Repositories.Interface;
+using ForumWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IPersoonRepository persoonRepo;
+        public AccountController(IPersoonRepository persoonRepo)
+        {
+            this.persoonRepo = persoonRepo;
+        }
+        public IActionResult Index()
+        {
+            AccountViewModel model = new AccountViewModel();
+            model.Gebruiker = HttpContext.Session.GetObject<Persoon>("Gebruiker");
+            return View(model);
+        }
+
+        public IActionResult Inloggen()
+        {
+            return View(new InloggenViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Inloggen(InloggenViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                Persoon gebruiker = persoonRepo.GetPersoonByLoginNaamAndPaswoord(model.Naam, model.Paswoord);
+                if (gebruiker is not null)
+                {
+                    HttpContext.Session.SetObject("Gebruiker", gebruiker);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(model);
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        public IActionResult Uitloggen()
+        {
+            return View();
+        }
+    }
+}
---
+++ ForumClient/SessionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumWeb
+{
+    //met dank aan https://www.talkingdotnet.com/store-complex-objects-in-asp-net-core-session/
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            string value = session.GetString(key);
+            return value is null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+        public static void SetBoolean(this ISession session, string key, bool value)
+        {
+            session.Set(key, BitConverter.GetBytes(value));
+        }
+
+        public static bool? GetBoolean(this ISession session, string key)
+        {
+            byte[] data = session.Get(key);
+            if (data is null)
+            {
+                return null;
+            }
+            return BitConverter.ToBoolean(data, 0);
+        }
+    }
+}
